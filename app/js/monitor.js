8002b0eee2baab3006dd26e4d61eea5996845b36
surajrocks1997 <surajrocks1997@gmail.com>
2020-09-09 11:31:26 +0530

    dynamic system stats added

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,23 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+// Run every 2 seconds
+setInterval(() => {
+  // cpu usage
+  cpu.usage().then((info) => {
+    document.getElementById('cpu-usage').innerHTML = info + '%';
+  });
+
+  // cpu free
+  cpu.free().then((info) => {
+    document.getElementById('cpu-free').innerText = info + '%';
+  });
+
+  // uptime
+  // console.log(os.uptime());
+  document.getElementById('sys-uptime').innerText = secondsToDHMS(os.uptime());
+}, 1500);
+
 // set model
 document.getElementById('cpu-model').innerText = cpu.model();
 
@@ -17,3 +34,12 @@ document.getElementById('os').innerText = `${os.type()} ${os.arch()}`;
 mem.info().then((info) => {
   document.getElementById('mem-total').innerText = `${info.totalMemMb} GB`;
 });
+
+const secondsToDHMS = (seconds) => {
+  seconds = +seconds;
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  return `${d}d ${h}h ${m}m ${s}s`;
+};
